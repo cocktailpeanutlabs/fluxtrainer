7bc416b74db52afcaf74b812e273494ee439546d
cocktailpeanut <cocktailpeanuts@proton.me>
2024-09-04 09:54:45 -0400

    update

--- install.js
+++ install.js
@@ -29,6 +29,7 @@ module.exports = {
         path: "app",                // Edit this to customize the path to start the shell from
         message: [
           "pip install -r requirements.txt",
+          "pip uninstall -y diffusers[torch]",
           "pip install -r ui_requirements.txt"
         ]
       }
